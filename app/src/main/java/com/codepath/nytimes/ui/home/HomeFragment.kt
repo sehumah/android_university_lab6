6b8c8601745df67db1733e7385a9626dc326a905
sehumah <sehumah@protonmail.com>
2022-03-27 17:31:31 -0500

    implement oncreateview() and inflate layout fragments

--- app/src/main/java/com/codepath/nytimes/ui/home/HomeFragment.kt
+++ app/src/main/java/com/codepath/nytimes/ui/home/HomeFragment.kt
@@ -1,8 +1,19 @@
 package com.codepath.nytimes.ui.home
 
+import com.codepath.nytimes.R
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
 
 class HomeFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // return super.onCreateView(inflater, container, savedInstanceState)
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+        return view
+    }
 
 }
--- app/src/main/java/com/codepath/nytimes/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/codepath/nytimes/ui/settings/SettingsFragment.kt
@@ -1,7 +1,19 @@
 package com.codepath.nytimes.ui.settings
 
+import com.codepath.nytimes.R
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
+
 class SettingsFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // return super.onCreateView(inflater, container, savedInstanceState)
+        val view = inflater.inflate(R.layout.fragment_settings, container, false)
+        return view
+    }
 
 }
