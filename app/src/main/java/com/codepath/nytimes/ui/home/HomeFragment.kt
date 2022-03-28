770d1110dac612ec00fcb4a840d87375ddfb2ea4
sehumah <sehumah@protonmail.com>
2022-03-27 20:26:33 -0500

    set the name of the fragment

--- app/src/main/java/com/codepath/nytimes/ui/home/HomeFragment.kt
+++ app/src/main/java/com/codepath/nytimes/ui/home/HomeFragment.kt
@@ -11,7 +11,7 @@ import androidx.fragment.app.Fragment
 class HomeFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        // return super.onCreateView(inflater, container, savedInstanceState)
+        activity?.title = getString(R.string.action_bar_home)
         val view = inflater.inflate(R.layout.fragment_home, container, false)
         return view
     }
--- app/src/main/java/com/codepath/nytimes/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/codepath/nytimes/ui/settings/SettingsFragment.kt
@@ -11,7 +11,7 @@ import androidx.fragment.app.Fragment
 class SettingsFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        // return super.onCreateView(inflater, container, savedInstanceState)
+        activity?.title = getString(R.string.action_bar_settings)
         val view = inflater.inflate(R.layout.fragment_settings, container, false)
         return view
     }
